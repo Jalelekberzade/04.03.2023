aec926a2f376f19a1eb1bd180ad61fbe0348d4a8
Jale Alekberzade <7p90g77@code.edu.az>
2023-04-05 13:35:51 +0400

    first commit

---
+++ Console Fight/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Console_Fight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        
+    }
+    public class Character 
+    {
+        public string Name;
+        public int Health;
+        public int AttackPower;
+
+        public void Attack() 
+        { 
+        }
+        public void TakeDamage() 
+        { 
+        }
+    }
+     
+    public class Player : Character
+    {
+        Attack();
+    }
+    public class Enemy : Character { }
+}
